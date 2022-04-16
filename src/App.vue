3633fa71b1a88a8c1407ac6a098e2c3d83e518c2
Christian <chnikel@gmail.com>
2022-04-16 14:54:21 +0200

    file splitted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import Card from "./components/Card.vue";
 import CardStack from "./components/CardStack.vue";
 import {
   stack,
@@ -8,29 +7,12 @@ import {
   dropToNewStack,
   craftDone,
 } from "./logic/game";
-import { CardRender, VillagerCardRender } from "./logic/canvas";
-
-onMounted(() => {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-
-  ctx.fillStyle = "#AFC5FF";
-  ctx.fillRect(0, 0, c.width, c.height);
-
-  new CardRender(50, 50).render(ctx, { compact: true });
-  new CardRender(50, 90).render(ctx, { compact: false });
-  new VillagerCardRender(50 + 200, 50).render(ctx);
-});
+import GameCanvas from "./components/GameCanvas.vue";
 </script>
 
 <template>
-  <canvas
-    id="myCanvas"
-    width="500"
-    height="400"
-    style="border: 1px solid #000000"
-  >
-  </canvas>
+  <game-canvas />
+
   <main
     class="bg-emerald-200 p-6 h-screen flex flex-wrap gap-3"
     @dragenter.prevent
---
+++ src/components/GameCanvas.vue
@@ -0,0 +1,40 @@
+
+<script setup>
+import { onMounted } from "vue";
+import { CardRender, VillagerCardRender } from "../logic/canvas";
+
+let ctx;
+let canvas;
+
+onMounted(() => {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+
+  render();
+});
+
+function render() {
+  ctx.fillStyle = "#AFC5FF";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  new CardRender(50, 50).render(ctx, { compact: true });
+  new CardRender(50, 90).render(ctx, { compact: false });
+  new VillagerCardRender(50 + 200, 50).render(ctx);
+}
+
+function click(event) {
+  console.log(event);
+
+  render();
+}
+</script>
+
+<template>
+  <canvas
+    id="myCanvas"
+    width="500"
+    height="400"
+    style="border: 1px solid #000000"
+    @click="click"
+  ></canvas>
+</template>

31d113e48e45c16fde0717fe51f836eedac83731
Christian <chnikel@gmail.com>
2022-04-16 14:37:30 +0200

    cleanup canvas renderer

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import {
   dropToNewStack,
   craftDone,
 } from "./logic/game";
+import { CardRender, VillagerCardRender } from "./logic/canvas";
 
 onMounted(() => {
   var c = document.getElementById("myCanvas");
@@ -16,56 +17,9 @@ onMounted(() => {
   ctx.fillStyle = "#AFC5FF";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  CanvasRenderingContext2D.prototype.roundRect = function roundRect(
-    x,
-    y,
-    w,
-    h,
-    r
-  ) {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
-    this.beginPath();
-    this.moveTo(x + r, y);
-    this.arcTo(x + w, y, x + w, y + h, r);
-    this.arcTo(x + w, y + h, x, y + h, r);
-    this.arcTo(x, y + h, x, y, r);
-    this.arcTo(x, y, x + w, y, r);
-    this.closePath();
-    return this;
-  };
-
-  drawCard(100, 50);
-  function drawCard(x, y) {
-    const width = 182;
-    const height = 227;
-    const headerHeight = 40;
-
-    // ground
-    ctx.fillStyle = "#FEF9C3";
-    ctx.roundRect(x, y, width, height, 5).fill();
-
-    // ground header
-    ctx.fillStyle = "#FEF08A";
-    ctx.roundRect(x, y, width, headerHeight, 5).fill();
-
-    // ground border
-    ctx.lineWidth = 4;
-    ctx.roundRect(x, y, width, height, 5).stroke();
-
-    // header border
-    ctx.beginPath();
-    ctx.moveTo(x, y + headerHeight);
-    ctx.lineTo(x + width, y + headerHeight);
-    ctx.stroke();
-
-    // title
-    ctx.fillStyle = "#000";
-    ctx.font = `bold 1.125rem ui-sans-serif, system-ui, Arial`;
-    ctx.textBaseline = "middle";
-
-    ctx.fillText("Villager", x + 10, y + 22);
-  }
+  new CardRender(50, 50).render(ctx, { compact: true });
+  new CardRender(50, 90).render(ctx, { compact: false });
+  new VillagerCardRender(50 + 200, 50).render(ctx);
 });
 </script>
 
---
+++ src/logic/canvas.js
@@ -0,0 +1,77 @@
+CanvasRenderingContext2D.prototype.roundRect = function roundRect(x, y, w, h, r) {
+	if (w < 2 * r) r = w / 2;
+	if (h < 2 * r) r = h / 2;
+	this.beginPath();
+	this.moveTo(x + r, y);
+	this.arcTo(x + w, y, x + w, y + h, r);
+	this.arcTo(x + w, y + h, x, y + h, r);
+	this.arcTo(x, y + h, x, y, r);
+	this.arcTo(x, y, x + w, y, r);
+	this.closePath();
+	return this;
+};
+
+class CardRender {
+	constructor(
+		x,
+		y,
+		title = "Placeholder",
+		primaryColor = "#61605B",
+		accentColor = "#43423D",
+		textColor = "#fff"
+	) {
+		this.x = x;
+		this.y = y;
+		this.title = title;
+		this.primaryColor = primaryColor;
+		this.accentColor = accentColor;
+		this.textColor = textColor;
+	}
+
+	render(ctx, { compact = false } = {}) {
+		const width = 182;
+		const headerHeight = 40;
+		const height = compact ? headerHeight : 227;
+		const borderRadius = 5;
+
+		// ground
+		ctx.fillStyle = this.accentColor;
+		ctx.roundRect(this.x, this.y, width, height, borderRadius).fill();
+
+		// ground header
+		ctx.fillStyle = this.primaryColor;
+		ctx.roundRect(this.x, this.y, width, headerHeight, borderRadius).fill();
+
+		// ground border
+		ctx.lineWidth = 4;
+		ctx.roundRect(this.x, this.y, width, height, borderRadius).stroke();
+
+		// header border
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y + headerHeight);
+		ctx.lineTo(this.x + width, this.y + headerHeight);
+		ctx.stroke();
+
+		// title
+		ctx.fillStyle = this.textColor;
+		ctx.font = `bold 1.125rem ui-sans-serif, system-ui, Arial`;
+		ctx.textBaseline = "middle";
+		ctx.fillText(this.title, this.x + 10, this.y + 22);
+
+		if (!compact) {
+			// body circle
+			ctx.fillStyle = this.primaryColor;
+			ctx.beginPath();
+			ctx.arc(this.x + width / 2, this.y + height / 2, 55, 0, 2 * Math.PI);
+			ctx.fill();
+		}
+	}
+}
+
+class VillagerCardRender extends CardRender {
+	constructor(x, y) {
+		super(x, y, "Villager", "#FEF08A", "#FEF9C3", "#000");
+	}
+}
+
+export { CardRender, VillagerCardRender };
